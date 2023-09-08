aaba858640a95df6ff79e5f281125a2d269dc277
Lara Wiesek <larawiesek83@gmail.com>
2023-09-07 22:42:57 -0300

    Preentrega2Wiesek

---
+++ productos.js
@@ -0,0 +1,27 @@
+const productos = [{
+        id: 1,
+        nombre: 'Poster personaje',
+        precio: 380
+
+    },
+    {
+        id: 2,
+        nombre: 'Vaso "I love coffee"',
+        precio: 2500
+    },
+    {
+        id: 3,
+        nombre: 'Posavasos "Galaxy"',
+        precio: 320
+    },
+    {
+        id: 4,
+        nombre: 'Taza "Personaje"',
+        precio: 600
+    },
+    {
+        id: 5,
+        nombre: 'Tote Bag "Andrómeda" ',
+        precio: 580
+    },
+];
